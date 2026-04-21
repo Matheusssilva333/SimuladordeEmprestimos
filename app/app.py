c855316a150b83a4c7811be69e8cb7608dbe145d
Matheus Silva <matheus1882014@outlook.com>
2026-04-21 14:40:33 -0300

    criando titulo da barra de pesquisa

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
-
+ 
 class BarraDePesquisa
 
-def __init__(self);
+def __init__ (self);
 self.altura = 3 cm
 self.largura = 5 cm
 self.comprimento = 5 cm
 
-def Pesquisar(self)
+def titulo (self)
+self.print(Pesquise aqui)
+
+def pesquisar (self)
 self.print("Digite aqui")
 
 barra_de_pesquisa = BarraDePesquisa
-print(barra_de_pesquisa.Pesquisar)
+print(barra_de_pesquisa.titulo.pesquisar)
+
+
+
